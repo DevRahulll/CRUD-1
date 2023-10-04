be23fa9f77aee706f31cb98950c0c1e6343071f5
Rahul <rahuldevan2019@gmail.com>
2023-10-05 00:58:03 +0530

    adding create user with error handling to the files

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -8,13 +8,22 @@ exports.createUser=async(req,res)=>{
     // extract info
     try {
         const{name,email}=req.body;
+        if(!name||!email){
+            throw new Error("Name and email are required");
+        }
+        
+        const userExist=User.findOne({email})
+        if(userExist){
+            throw new Error("User already exist");
+        }
         const user=await User.create({
             name,
             email
         })
         res.status(201).json({
             success:true,
-            message:"User Created Successfully"
+            message:"User Created Successfully",
+            user
         })
     } catch (error) {
         console.log(error);
